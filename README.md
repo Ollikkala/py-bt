0c42c066b6ae5c84314526820582b1b087c4ae24
Olli <olli@pop-os.localdomain>
2019-10-11 08:25:46 +0200

    Corrected spelling

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Rename to bt, make a link in the /usr/bin folder to
+# Rename bt.py to bt, make a link in the /usr/bin folder to
 # make it a command.
--- bt.py
+++ bt.py
@@ -10,5 +10,5 @@ info = os.system('bluetoothctl connect 5C:EB:68:75:78:4D')
 # Setting sound quality to A2DP
 os.system('pacmd set-card-profile bluez_card.5C_EB_68_75_78_4D a2dp_sink')
 
-# Sets volume to 50%
+# Sets volume to 60%
 os.system('amixer -D pulse sset Master 60%')
